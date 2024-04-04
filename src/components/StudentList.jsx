c63b85e9ad7d45eb645a3d679f6444ee9430afc7
Deep Kothari <deepk2891@gmail.com>
2024-04-04 14:10:36 +0530

    Fetch Data from Realtime Database

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,7 +1,36 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { getDatabase, onValue, ref } from "firebase/database"
+import app from "../Firebase"
 
 const StudentList = () => {
-	return <div>StudentList</div>
+	const [studentData, setStudentData] = useState(null)
+	useEffect(() => {
+		const db = getDatabase(app)
+		const studentRef = ref(db, "student")
+		onValue(studentRef, (snapshot) => {
+			const data = snapshot.val()
+			console.log(data)
+			setStudentData(data)
+		})
+	}, [])
+
+	return (
+		<div>
+			<h1>StudentList</h1>
+			{studentData && (
+				<div>
+					{Object.entries(studentData).map(([key, value]) => {
+						return (
+							<div key={key} className="flex gap-5">
+								<p>Name : {value.userName}</p>
+								<p>Password : {value.password}</p>
+							</div>
+						)
+					})}
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default StudentList
